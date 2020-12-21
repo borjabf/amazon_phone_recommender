8d775447308fe6ef9453800e0f9e1f989a9ec3d9
borjabf <58516002+borjabf@users.noreply.github.com>
2020-12-21 17:02:31 +0100

    query script

---
+++ top_ten_phones.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient 
+
+client = MongoClient("mongodb+srv://Borja:b4zfdJFP9QQ80LGY@amazon.nbiae.mongodb.net/Amazon?retryWrites=true&w=majority")
+
+precio_usuario = int(input("¿Cuánto quieres gastarte como máximo?"))
+
+result = list(
+    client["Amazon"]["Productos"].aggregate(
+        [
+            {
+                "$match": {
+                    "Precio": {"$lte": precio_usuario},
+                    "Score": {"$exists": True},
+                }
+            },
+            {"$sort": {"Score": -1}},
+            {"$limit": 10},
+        ]
+    )
+)
+
+for resultado in result:
+    if resultado["Marca"] is None:
+        resultado["Marca"] = "Marca sin especificar"
+    print("###################################################################")
+    print(("Nombre:{nombre_result}").format(nombre_result=resultado["Nombre"]))
+    print(("Precio:{precio_result}").format(precio_result=resultado["Precio"]))
+    print(("Marca:{marca_result}").format(marca_result=resultado["Marca"]))
+    print(("Media:{media_result}").format(media_result=resultado["media"]))
+    print(("url:{url_result}").format(url_result=resultado["url"]))
